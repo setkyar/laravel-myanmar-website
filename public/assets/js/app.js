ef11240cbd619d77d57dac3e8cb30a9b7d09d10e
Hein Zaw Htet <maungheinzawhtet@gmail.com>
2014-07-21 11:56:03 +0630

    Fix font for mac

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
 	// Fix font issue for MAC
-	// dirty code :3
 	var OSName="Other";
 	if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
 	if (OSName == "MacOS") {
         $.ajax({
-            url:"http://fonts.googleapis.com/earlyaccess/tharlon.css",
+            url:"http://localhost/laravel-mm/public/assets/css/tharlon.css",
             // dataType:"script",
             success:function(data){
                  $("head").append("<style>" + data + "</style>");
-                 $('body').css('font-family', 'Tharlon');
             }
         });
 	}
