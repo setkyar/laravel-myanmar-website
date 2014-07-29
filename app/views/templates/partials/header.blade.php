402476f830017831372d2bd5464bf2bbd41db335
Hein Zaw Htet <maungheinzawhtet@gmail.com>
2014-07-29 17:34:58 +0630

    added FAQs and other minor fixes

--- app/views/templates/partials/header.blade.php
+++ app/views/templates/partials/header.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{ asset('assets/img/lm-icon.ico') }}" type="image/x-icon" />
+    <link rel="shortcut icon" href="{{ asset('assets/img/lm-icon.ico') }}" type="image/x-icon" />
+
     <title>{{ isset($pageTitle) ? $pageTitle . " - Laravel Myanmar" : 'Laravel Myanmar' }}</title>
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
