5722c8bde14b415cc8e0e48d784d3984872c5788
Set Kyar Wa Lar <setkyar16@gmail.com>
2015-01-24 02:48:58 +0630

    Added Twitter on Single

--- app/views/blogs/single.blade.php
+++ app/views/blogs/single.blade.php
@@ -1,12 +1,20 @@
 @extends('templates.layouts.default')
 
 @section('content')
+<style type="text/css">
+  #twitter-widget-0 {
+    height: 34px !important;
+  }  
+</style>
 
 <section id="main-body" class="row">
   <div class="parent-page-title col-md-12">
     <h1>{{ $blog->title }}</h1>
     <h4 class="blog-meta"><i class="glyphicon glyphicon-user"></i> <a href="{{ route('profile', array($blog->user['profile_url'])) }}">{{ $blog->user->username }}</a> <i class="glyphicon glyphicon-calendar"></i> {{ $blog->created_at->format('d F Y') }} </h4>
     <div class="social-box">
+      <a href="https://twitter.com/share" class="twitter-share-button" data-text="http://laravelmyanmar.com/blog" data-via="Laravel_Myanmar" data-hashtags="Laravel_Myanmar" data-dnt="true">Tweet</a>
+      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
       <iframe src="//www.facebook.com/plugins/like.php?href={{ route('blogSingle', array($blog->slug) ); }}&amp;width=150px&amp;layout=standard&amp;action=like&amp;show_faces=false&amp;share=true&amp;height=35&amp;appId=195310000651099" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150pxpx; height:35px;" allowTransparency="true"></iframe>
     </div>
   </div>
